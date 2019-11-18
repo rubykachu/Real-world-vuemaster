71debbbdbe51bbf81a371948216531c8bd0cd12e
at-minhtang <minh.tang@asiantech.vn>
2019-11-18 16:08:02 +0700

    init app

---
+++ src/views/EventList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Even listing</h1>
+    <EventCard />
+    <BaseIcon />
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  components: {
+    EventCard
+  }
+};
+</script>
